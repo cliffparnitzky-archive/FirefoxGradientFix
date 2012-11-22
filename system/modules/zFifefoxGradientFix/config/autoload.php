18edf3a0b1c86c2043eda11ca56058da7b8f76b3
Cliff Parnitzky <github@cliff-parnitzky.de>
2012-11-22 17:16:45 +0100

    Added Contao 3 compatility

---
+++ system/modules/zFifefoxGradientFix/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package ZFifefoxGradientFix
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'FifefoxGradientFix' => 'system/modules/zFifefoxGradientFix/FifefoxGradientFix.php',
+));
